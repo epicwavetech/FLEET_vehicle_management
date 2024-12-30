2064731730b8191f9f58726960a38404b5b900b3
EpicWave Technologies <epicwavetechnologies@gmail.com>
2024-12-31 04:12:55 +0530

    Update s

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -53,4 +53,4 @@ export const useStore = create(
 );
 
 // export const SERVER_URL = "http://localhost:3000";
-export const SERVER_URL = "https://vehicle-management-server-axkf.onrender.com";
+export const SERVER_URL = "https://fleet-epicwave.onrender.com";
